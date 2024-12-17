f1a2e663a2d1ef7d6834b02e12c2f6ed689dcd4b
Blanche Cérémonie <130890116+Surtalaevateinn@users.noreply.github.com>
2024-12-18 02:09:13 +0800

    Update index.php
    Redesigned

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if ($isLoggedIn) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Housing Management</title>
-    <link rel="stylesheet" href="styleindex.css">
+    <link rel="stylesheet" href="css/styleindex.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
