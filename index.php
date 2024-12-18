a97a88685cbbd3b32ce17b6cf373312e53905435
Blanche Cérémonie <130890116+Surtalaevateinn@users.noreply.github.com>
2024-12-18 16:31:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -281,9 +281,9 @@
     <ul class="footer-links">
         <li><a href="#">About us</a></li>
         <li><a href="#">Cooperation</a></li>
-        <li><a href="#">Agreement</a></li>
-        <li><a href="#">Contact us</a></li>
-        <li><a href="#">FAQ</a></li>
+        <li><a href="Terms and Conditions">Agreement</a></li>
+        <li><a href="contactme.php">Contact us</a></li>
+        <li><a href="faq.php">FAQ</a></li>
     </ul>
     <ul class="social-icons">
         <li><a href="#">X</a></li>
