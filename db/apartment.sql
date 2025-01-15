5299dc61eed7778cb5a9211e40a35a5ab2032f24
abel <abel@hotcoin.biz>
2025-01-15 15:02:08 +0100

    recreate sql

--- db/apartment.sql
+++ db/apartment.sql
@@ -1,110 +1,94 @@
-/*
- Navicat Premium Data Transfer
 
- Source Server         : localhost_3307
- Source Server Type    : MySQL
- Source Server Version : 50743 (5.7.43-log)
- Source Host           : localhost:3307
- Source Schema         : apartment
-
- Target Server Type    : MySQL
- Target Server Version : 50743 (5.7.43-log)
- File Encoding         : 65001
-
- Date: 20/11/2024 22:02:30
-*/
-
--- 判断数据库是否存在，如果不存在则创建
 CREATE DATABASE IF NOT EXISTS apartment;
-
--- 使用该数据库
 USE apartment;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+create table if not exists admin
+(
+    Idadmin   int          not null,
+    Adminname varchar(255) not null,
+    Adminpswd varchar(255) not null,
+    constraint `PRIMARY`
+        primary key (Idadmin)
+)
+    engine = InnoDB
+    charset = utf8
+    row_format = DYNAMIC;
 
--- ----------------------------
--- Table structure for admin
--- ----------------------------
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE `admin`  (
-  `Idadmin` int(11) NOT NULL,
-  `Adminname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `Adminpswd` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`Idadmin`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+create table if not exists contract
+(
+    Idcontract int auto_increment constraint `PRIMARY`
+			primary key,
+    Signdate   datetime not null,
+    Startdate  datetime not null,
+    Enddate    datetime not null,
+    Iduser     int      not null
+)
+    engine = InnoDB
+    charset = utf8
+    row_format = DYNAMIC;
 
--- ----------------------------
--- Records of admin
--- ----------------------------
+create table if not exists favorites
+(
+    id         int auto_increment constraint `PRIMARY`
+			primary key,
+    user_id    int                                 not null,
+    room_id    int                                 not null,
+    created_at timestamp default CURRENT_TIMESTAMP not null,
+    constraint unique_favorite
+        unique (user_id, room_id)
+);
 
--- ----------------------------
--- Table structure for contract
--- ----------------------------
-DROP TABLE IF EXISTS `contract`;
-CREATE TABLE `contract`  (
-  `Idcontract` int(11) NOT NULL AUTO_INCREMENT,
-  `Signdate` datetime NOT NULL,
-  `Startdate` datetime NOT NULL,
-  `Enddate` datetime NOT NULL,
-  `Iduser` int(11) NOT NULL,
-  PRIMARY KEY (`Idcontract`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+create table if not exists picture
+(
+    id      int      not null,
+    room_id int      not null,
+    content longtext not null,
+    constraint `PRIMARY`
+        primary key (id)
+);
 
--- ----------------------------
--- Records of contract
--- ----------------------------
+create table if not exists profile
+(
+    Idprofile int auto_increment constraint `PRIMARY`
+			primary key,
+    phonenum  varchar(255) not null,
+    address   varchar(255) not null,
+    rib       varchar(255) not null
+)
+    engine = InnoDB
+    charset = utf8
+    row_format = DYNAMIC;
+
+create table if not exists room
+(
+    room_id      int auto_increment constraint `PRIMARY`
+			primary key,
+    capacity     int          not null,
+    description  varchar(255) not null,
+    is_available char         not null,
+    user_id      int          not null,
+    title        varchar(200) null,
+    persons      varchar(100) null,
+    price        varchar(100) null,
+    type         varchar(100) null,
+    location     varchar(200) null
+)
+    engine = InnoDB
+    charset = utf8
+    auto_increment = 11
+    row_format = DYNAMIC;
+
+create table if not exists user
+(
+    Iduser   int(10) auto_increment constraint `PRIMARY`
+			primary key,
+    Username varchar(255) not null,
+    Password varchar(255) not null,
+    Email    varchar(255) not null
+)
+    engine = InnoDB
+    charset = utf8
+    auto_increment = 2
+    row_format = DYNAMIC;
 
 
-
--- ----------------------------
--- Table structure for profile
--- ----------------------------
-DROP TABLE IF EXISTS `profile`;
-CREATE TABLE `profile`  (
-  `Idprofile` int(11) NOT NULL AUTO_INCREMENT,
-  `phonenum` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `rib` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`Idprofile`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of profile
--- ----------------------------
-
--- ----------------------------
--- Table structure for room
--- ----------------------------
-DROP TABLE IF EXISTS `room`;
-CREATE TABLE `room`  (
-  `Idroom` int(11) NOT NULL AUTO_INCREMENT,
-  `Capacity` int(11) NOT NULL,
-  `Description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `Isavailable` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `Iduser` int(11) NOT NULL,
-  PRIMARY KEY (`Idroom`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of room
--- ----------------------------
-
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
-  `Iduser` int(10) NOT NULL AUTO_INCREMENT,
-  `Username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `Password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `Email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`Iduser`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES (1, 'ZDS', '1234', '1476475770@qq.com');
-
-SET FOREIGN_KEY_CHECKS = 1;
