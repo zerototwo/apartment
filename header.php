ae54b8677bce473e3578834dced6827981c517d2
Blanche Cérémonie <130890116+Surtalaevateinn@users.noreply.github.com>
2025-01-15 22:38:00 +0800

    Update header.php

--- header.php
+++ header.php
@@ -8,10 +8,10 @@
             <li><a href="index.php">Home</a></li>
             <li><a href="#">Apartment</a></li>
             <li><a href="favorite.php">My Intention</a></li>
-            <li><a href="#">Additional Service</a></li>
+            <li><a href="addiservice.php">Additional Service</a></li>
             <li><a href="login.php">Log in</a></li>
             <li><a href="register.php">Sign up</a></li>
-            <li><a href="#">Help</a></li>
+            <li><a href="contract.php">Help</a></li>
         </ul>
     </nav>
 </header>
