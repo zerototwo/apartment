05c451cd085f4ca9080df13f5000ac3c6cae5107
Blanche Cérémonie <130890116+Surtalaevateinn@users.noreply.github.com>
2025-01-16 21:07:49 +0800

    Update header.php

--- header.php
+++ header.php
@@ -20,7 +20,17 @@ if ($isLoggedIn) {
 }
 ?>
 
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Housing Management</title>
+    <link rel="stylesheet" href="css/styleindex.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+</head>
 
 <header>
     <!-- Logo -->
